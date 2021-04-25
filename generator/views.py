164728c28d56ff7dc1c0a50d65119a63b74d29d9
Shiva KC <kcshiva9898@gmail.com>
2021-04-25 11:01:05 +1000

    First Commit

---
+++ generator/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import random
+
+# Create your views here.
+def home(request):
+    return render(request,'generator/home.html')
+
+def name(request):
+     return HttpResponse(f'This site is created by Shiva KC')
+
+def password(request):
+
+    characters = list('abcdefghijklmnopqrstuvwxyz')
+    if request.GET.get('Uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+    if request.GET.get('Special Character'):
+        characters.extend(list('!@#$%^&*()'))
+    if request.GET.get('Numbers'):
+        characters.extend(list('0123456789'))
+
+    length = int(request.GET.get('length'))
+    thepassword = ''
+    for x in range (length):
+        thepassword += random.choice(characters)
+    return render(request,'generator/password.html',{'password':thepassword})
+def About(request):
+    return render(request,'generator/About.html')
